9d9bf0582e658e1047f1392911ccf2bdb2063bb3
Remi Rousselet <darky12s@gmail.com>
2018-10-23 19:39:21 +0200

    release

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import "element-ui/lib/theme-chalk/index.css";
 import Vue from "vue";
 import Router from "vue-router";
 import App from "./App.vue";
-import "./registerServiceWorker";
 import router from "./router";
 
 Vue.config.productionTip = false;
